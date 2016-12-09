5be1faba64c9a420330b2c099a647382497c1430
yonikan <yonikan@gmail.com>
2016-12-09 23:49:15 +0200

    My init (little whitespaces changes).

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
+
+import { Recipe } from "../recipe";
+import { ShoppingListService } from "../../shopping-list";
+import { RecipeService } from "../recipe.service";
+
+@Component({
+    selector: 'rb-recipe-detail',
+    templateUrl: 'recipe-detail.component.html'
+})
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+    private subscription: Subscription;
+    private recipeIndex: number;
+    selectedRecipe: Recipe;
+
+    constructor(private sls: ShoppingListService,
+                private route: ActivatedRoute,
+                private recipesService: RecipeService,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        this.subscription = this.route.params.subscribe(
+            (params: any) => {
+                this.recipeIndex = params['id'];
+                this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+            }
+        );
+    }
+
+    onEdit() {
+        this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+    }
+
+    onDelete() {
+        this.recipesService.deleteRecipe(this.selectedRecipe);
+        this.router.navigate(['/recipes']);
+    }
+
+    onAddToShoppingList() {
+        this.sls.addItems(this.selectedRecipe.ingredients);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+}
